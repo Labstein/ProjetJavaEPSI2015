295a6078ac7914d0f1e5f072d7440c2fb2960ce8
Vahati <adrien-lt@hotmail.fr>
2015-11-20 09:53:12 +0100

    Debut Thread

--- src/fr/epsi/model/thread/ThreadImageAlea.java
+++ src/fr/epsi/model/thread/ThreadImageAlea.java
@@ -1,5 +1,17 @@
 package fr.epsi.model.thread;
-
+import fr.epsi.util.Alea;
+import fr.epsi.model.Afficheur;
 public class ThreadImageAlea extends Thread {
 	
+	public void run() {
+		long start = System.currentTimeMillis();
+		
+		for (int i = 0; i <= Afficheur.getListeCamera().size(); i++) {
+			
+		}
+			
+		while (System.currentTimeMillis() < (start + 5000)) {
+			
+		}
+	}
 }
