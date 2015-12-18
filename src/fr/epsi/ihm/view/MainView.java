6d078ec530757eed073b36f087c2fd26ce19935b
Vahati <adrien-lt@hotmail.fr>
2015-12-18 15:54:03 +0100

    ajout fxml

--- src/fr/epsi/ihm/view/MainView.java
+++ src/fr/epsi/ihm/view/MainView.java
@@ -7,6 +7,9 @@ import fr.epsi.io.CameraReader;
 import fr.epsi.model.Afficheur;
 import fr.epsi.model.Camera;
 import fr.epsi.model.Ecran;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class MainView extends javafx.application.Application {
@@ -15,6 +18,14 @@ public class MainView extends javafx.application.Application {
 	
 	@Override
 	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = FXMLLoader.load(getClass().getResource("projetJavaFxml.fxml"));
+			Scene sc = new Scene(root);
+			primaryStage.setScene(sc);
+		} catch (IOException e1) {
+			System.err.println("Impossibilité de charger l'interface.");
+			e1.printStackTrace();
+		}
 		primaryStage.setTitle("Falcon Watch");
 		try {
 			Afficheur mainAfficheur = new Afficheur(new Ecran[NB_MAX_MONITEURS], new ArrayList<Camera>(CameraReader.getAllCamera()));
