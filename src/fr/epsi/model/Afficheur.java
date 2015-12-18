74bfdf5037a6a7d3e4a2e745a2bfbb0f3a3c630d
Labstein <asile.labyrinthus@gmail.com>
2015-12-18 15:54:58 +0100

    Update Afficheur
    Possibilité de récupérer les écrans

--- src/fr/epsi/model/Afficheur.java
+++ src/fr/epsi/model/Afficheur.java
@@ -10,6 +10,9 @@ public class Afficheur {
 	public static List<Camera> getListeCamera(){
 		return listeCamera;
 	}
+	public Ecran[] getTabEcran(){
+		return tabEcran;
+	}
 	//Constructeurs
 	public Afficheur(Ecran[] tabEcran, List<Camera> listeCamera){
 		this.tabEcran = tabEcran;
